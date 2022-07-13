7b8e7a0d5812f085d93446001993c4922265f8bd
topneon <44845264+topneon@users.noreply.github.com>
2022-07-13 23:49:17 +0200

    Delete Scripts directory

--- Scripts/Bootcamps.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Bootcamps : MonoBehaviour
-{
-    public List<Bootcamp> bootcamps;
-}
--- Scripts/MatchGrid.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class MatchGrid : MonoBehaviour
-{
-    public Text team1, team2, points1, points2;
-}
